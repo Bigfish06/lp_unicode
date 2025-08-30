1a2b839c911bd8a27b64c93a3fcc781b8ce34d9f
Keenan <keenanmenezes2006@gmail.com>
2025-08-30 22:01:36 +0530

    Update project 1 with new features

--- 1/app.js
+++ 1/app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app=express()
 const mongoose=require("mongoose")
+const morgan=require('morgan')
 require('dotenv').config()
 
 const address=process.env.mongoDB_url
@@ -17,6 +18,13 @@ app.listen(5000,()=>{
 })
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(morgan('tiny'))
+
+//or we can specify what to store(a whole list of things):-
+// morgan.token("host",(req,res)=>{
+//     return req.hostname
+// })
+// app.use(morgan(':method :url :host'))
 
 const router=require('./routers/router')
 app.use('/api/user',router)
